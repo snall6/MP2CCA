1f04091cf60d9fff938f85436ede885219a03a84
Sailaja <90356296+snall6@users.noreply.github.com>
2024-01-27 07:18:27 +0530

    Create serve.py

---
+++ serve.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+import subprocess
+import socket
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def handle_request():
+    if request.method == 'POST':
+        subprocess.Popen(['python', 'stress_cpu.py'])
+        return 'Stressing CPU initiated\n', 200
+    elif request.method == 'GET':
+        private_ip = socket.gethostbyname(socket.gethostname())
+        return f'Private IP address: {private_ip}\n', 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
